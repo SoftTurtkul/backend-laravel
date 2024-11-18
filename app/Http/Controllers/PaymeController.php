cd4130887d2c735cf8fa52343f4af14ac732cc1f
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-11-19 01:11:21 +0500

    ws

--- app/Http/Controllers/PaymeController.php
+++ app/Http/Controllers/PaymeController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 class PaymeController extends Controller
 {
-    const test_token="Drb4rksirx6NhWqJa@R%YeSj3jC&aTRKUbQK";
-    const user="Paycom";
+    const test_token = "Drb4rksirx6NhWqJa@R%YeSj3jC&aTRKUbQK";
+    const user = "Paycom";
+
     public function index()
     {
-        return \request()->header('Authorization');
+        $auth = \request()->header('Authorization');
+        $token = str_replace("Bearer ", "", $auth);
+        if (!base64_encode(self::user . ":" . self::test_token) == $token) {
+            return [
+                "error" => [
+                    "code" => -31050,
+                ]
+            ];
+        }
         $data = \request()->toArray();
         switch ($data['method']) {
             case 'CheckPerformTransaction':
