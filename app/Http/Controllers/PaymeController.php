9c77be42f33685ad070db52df27acc4ca2e5e82b
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-11-19 01:24:51 +0500

    paye

--- app/Http/Controllers/PaymeController.php
+++ app/Http/Controllers/PaymeController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Order;
+
 class PaymeController extends Controller
 {
     const test_token = "Drb4rksirx6NhWqJa@R%YeSj3jC&aTRKUbQK";
@@ -11,15 +14,10 @@ class PaymeController extends Controller
         $auth = \request()->header('Authorization');
         $token = str_replace("Basic ", "", $auth);
         if (base64_encode(self::user . ":" . self::test_token) != $token) {
-            return json_encode([
-                "error" => [
-                    "code" => -32504,
-                    "message"=>[
-                        "ru"=>"Not Allowed",
-                        "en"=>"Not Allowed",
-                        "uz"=>"Not Allowed",
-                    ]
-                ]
+            return $this->Error(-32504, [
+                "ru" => "Not Allowed",
+                "en" => "Not Allowed",
+                "uz" => "Not Allowed",
             ]);
         }
         $data = \request()->toArray();
@@ -30,9 +28,26 @@ class PaymeController extends Controller
         }
     }
 
-    private function CheckPerformTransaction(array $params)
+    private function CheckPerformTransaction(array $params):string
     {
+        $order = Order::query()->where('id', $params['account']['order_id'])->first();
+        if (!$order) {
+            return $this->Error(-31050, [
+                'en' => "Order not found",
+                "ru" => "Order not found",
+                "uz" => "Order not found",
+            ]);
+        }
+    }
 
+    private function Error($code, $message)
+    {
+        return json_encode([
+            "error" => [
+                "code" => $code,
+                "message" => $message
+            ]
+        ]);
     }
 
 }
