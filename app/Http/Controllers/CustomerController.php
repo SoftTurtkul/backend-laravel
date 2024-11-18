9382a6bd3259ddbbf87dae1da0051e23c552bdcf
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-11-18 14:16:45 +0500

    Add delete consumer endpoint

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,34 +5,49 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CustomerRequest;
 use App\Http\Services\CustomerService;
 use App\Models\Customer;
+use Illuminate\Support\Facades\Auth;
 
-class CustomerController extends Controller {
+class CustomerController extends Controller
+{
 
     /**
      * @var CustomerService
      */
     private CustomerService $service;
 
-    public function __construct(CustomerService $service) {
+    public function __construct(CustomerService $service)
+    {
         $this->service = $service;
     }
-    public function index() {
+
+    public function index()
+    {
         return $this->service->all();
     }
 
-    public function store(CustomerRequest $request) {
+    public function store(CustomerRequest $request)
+    {
         return $this->service->save($request);
     }
 
-    public function show(Customer $customer) {
+    public function show(Customer $customer)
+    {
         return $this->success($customer);
     }
 
-    public function update(CustomerRequest $request, Customer $customer) {
+    public function update(CustomerRequest $request, Customer $customer)
+    {
         return $this->service->update($customer, $request->validated());
     }
 
-    public function destroy(Customer $customer) {
+    public function destroy(Customer $customer)
+    {
         return $this->service->destroy($customer);
+    }
+
+    public function delete()
+    {
+        $model = Customer::query()->where('id', \auth('sanctum')->user()->id)->first();
+        return $this->service->destroy($model);
     }
 }
