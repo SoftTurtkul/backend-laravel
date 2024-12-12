545f6f9d82dfc66f1413e7a94c6a1527ea0662e0
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-12-12 11:39:27 +0500

    fix

--- app/Http/Services/PartnerService.php
+++ app/Http/Services/PartnerService.php
@@ -32,7 +32,7 @@ class PartnerService extends CRUDService
             $data['img'] = $this->saveImage($request->file('img'), 'partners');
             $this->deleteFile($partner->img, 'partners');
         }
-        dd($data,$request->all());
+        dd($data,$request->toArray());
         return parent::update($partner, $data);
     }
 
