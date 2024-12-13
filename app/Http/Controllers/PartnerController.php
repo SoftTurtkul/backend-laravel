014a6146a814b19114d2cdace14cac701ae5001b
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-12-13 16:12:14 +0500

    2)❗️Partners/stats/filter data range orqali hisoblash (https://t.me/c/2444198264/1/3591)

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -67,14 +67,14 @@ class PartnerController extends Controller
             ->select(
                 'orders.partner_id',
                 'partners.name as partner_name',
-                DB::raw('COUNT(CASE WHEN DATE(histories.created_at) = ' . $today . ' THEN 1 END) AS daily_count'),
+                DB::raw("COUNT(CASE WHEN DATE(histories.created_at) = '" . $today . "' THEN 1 END) AS daily_count"),
                 DB::raw('COUNT(CASE WHEN YEAR(histories.created_at) = YEAR(CURDATE()) AND MONTH(histories.created_at) = MONTH(CURDATE()) THEN 1 END) AS monthly_count'),
                 DB::raw('COUNT(CASE WHEN YEAR(histories.created_at) = YEAR(CURDATE()) THEN 1 END) AS yearly_count'),
-                DB::raw('SUM(CASE WHEN DATE(histories.created_at) = ' . $today . ' THEN (orders.total_price - orders.delivery_price) ELSE 0 END) AS daily_sum'),
+                DB::raw("SUM(CASE WHEN DATE(histories.created_at) = '" . $today . "' THEN (orders.total_price - orders.delivery_price) ELSE 0 END) AS daily_sum"),
                 DB::raw('SUM(CASE WHEN YEAR(histories.created_at) = YEAR(CURDATE()) AND MONTH(histories.created_at) = MONTH(CURDATE()) THEN (orders.total_price - orders.delivery_price) ELSE 0 END) AS monthly_sum'),
                 DB::raw('SUM(CASE WHEN YEAR(histories.created_at) = YEAR(CURDATE()) THEN (orders.total_price - orders.delivery_price) ELSE 0 END) AS yearly_sum')
             )
-            ->where('histories.status', 2)->dd()
+            ->where('histories.status', 2)
         ;  // Filter by status = 2
         return $this->indexResponse($query
             ->groupBy('orders.partner_id', 'partners.name')  // Group by partner_id and partner name
