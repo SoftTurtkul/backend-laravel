c9453b68b412c110e1a968857d58ae5e4e3b29fc
QalandarDev <Q.Axmedov@uzinfocom.uz>
2024-12-15 20:16:16 +0500

    add driver to partner/orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -102,6 +102,9 @@ class OrderController extends Controller
                 ]);
                 if($inserted) {
                     Product::query()->where('id', $item['product_id'])->decrement('quantity', $item['quantity']);
+                    if(Product::query()->where('id', $item['product_id'])->first()->quantity=0) {
+                        Product::query()->where('id', $item['product_id'])->update(['status' => 0]);
+                    }
                 }
             }
 
